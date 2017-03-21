046e8b2f44e710c5876ba6d1a85a855631db4659
mozevegas <moevegas@gmail.com>
2017-03-21 13:49:46 -0400

    Basic Randomizer done

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -10,6 +10,26 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
+
+            Random rndTwo = new Random();
+            int randomNumber = rndTwo.Next(0, 100);
+
+            var guessMe = 22;
+            Console.WriteLine($"this is the random, shhhh {randomNumber}");
+            Console.WriteLine(guessMe);
+            if (guessMe == 50)
+            {
+                Console.WriteLine("Correct number");
+            }
+            else if (guessMe == 48)
+            {
+                Console.WriteLine("number 48");
+            }
+             
+            else
+            {
+                Console.WriteLine("the key was 22");
+            }
         }
     }
 }
